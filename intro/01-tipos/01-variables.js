7f34419281e0d181a6a085d910fd31a2fe79f1fb
Juan Luis Mosqueda Orta <122045310@upq.edu.mx>
2024-05-08 15:25:51 -0600

    Reglas para nombrar variables

---
+++ intro/01-tipos/01-variables.js
@@ -0,0 +1,10 @@
+
+let nombre = 'Hola Mundo';
+let NombreCompleto; // UpperCamelCase
+let nombreCompleto; // CamelCase
+let nombre_completo; // SnakeCase
+console.log(NombreCompleto);
+
+let apellido;
+let animal;
+apellido = 'Mosqueda';
