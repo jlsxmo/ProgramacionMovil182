55668e068538fef5ae5ef2ce50e7e84c8e96dea6
Juan Luis Mosqueda Orta <122045310@upq.edu.mx>
2024-05-08 23:50:07 -0600

    Arrays

---
+++ intro/01-tipos/07-arrays.js
@@ -0,0 +1,12 @@
+let animales = ['chanchito', 'caballo']; // Array literal
+console.log(animales);
+console.log(animales[0]);
+
+// Modificar elementos
+animales[2] = 'dragón';
+console.log(animales);
+
+animales[10] = 'Pez';
+console.log(animales[7]);
+console.log(typeof animales);
+console.log(animales.length);
