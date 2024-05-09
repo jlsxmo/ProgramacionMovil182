02519720bb050a174aea5a3f4898f51f06e08d77
Juan Luis Mosqueda Orta <122045310@upq.edu.mx>
2024-05-08 23:26:47 -0600

    Tipado dinámico

---
+++ intro/01-tipos/04-tipado-dinamico.js
@@ -0,0 +1,11 @@
+let numero = 42;
+let nombre = "Hola Mundo";
+let verdadero = true;
+let undef;
+let nula = null;
+
+console.log(typeof numero); // number | typeof nos permite saber el tipo de dato de una variable
+console.log(typeof nombre); // string
+console.log(typeof verdadero); // boolean
+console.log(typeof undef); // undefined
+console.log(typeof nula); // Nos dirá que es un objeto, ya que se considera un objeto nulo
