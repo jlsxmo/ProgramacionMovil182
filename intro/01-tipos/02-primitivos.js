cfeafc48e77b0d88f6ab461b218feb4b8e528561
Juan Luis Mosqueda Orta <122045310@upq.edu.mx>
2024-05-08 23:07:52 -0600

    Tipos de datos
    - Tipos primitivos (stack - RAM - estáticos)
    - De referencia (heap - RAM - dinámicos)

---
+++ intro/01-tipos/02-primitivos.js
@@ -0,0 +1,6 @@
+let numero = 1; // Númerico
+let texto = 'Hola mundo'; // String
+let verdadero = true; // Boolean
+let falso = false; // Boolean
+let noDefinido = undefined; // Undefined (Tipo de dato | Valor)
+let nulo = null; // Null (Tipo de dato | Valor)
