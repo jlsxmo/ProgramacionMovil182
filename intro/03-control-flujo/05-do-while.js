c6eea329ff15f13671a9afb6655e3c0fc6d07e87
Juan Luis Mosqueda Orta <122045310@upq.edu.mx>
2024-05-10 18:05:51 -0600

    Control flujo pt1
    - if/else
    - bucles: while/do-while/for/for-of

---
+++ intro/03-control-flujo/05-do-while.js
@@ -0,0 +1,16 @@
+let i = 2;
+/*
+while (i < 10){
+    if (i % 2 == 0){
+        console.log('Número par: ' + i);
+    }
+    i++;
+}
+*/
+
+do {
+    if (i % 2 == 0){
+        console.log('Número par: ' + i);
+    }
+    i++;
+} while (i < 2);
---
+++ intro/03-control-flujo/07-for-of.js
@@ -0,0 +1,5 @@
+let animales = ['perro', 'gato', 'pez', 'loro', 'tortuga'];
+
+for (let animal of animales){
+    console.log(animal);
+}
