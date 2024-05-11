126c032742e620a535a37b581ad9624b817a0fc4
Juan Luis Mosqueda Orta <122045310@upq.edu.mx>
2024-05-10 20:11:09 -0600

    Ejercicio 7

---
+++ intro/04-ejercicios/07.js
@@ -0,0 +1,9 @@
+/**
+ * Ejercicio 07
+ * Crear algoritmo que devuelva el precio del producto más impuesto
+ */
+function precioCompleto(precio, impuesto){
+    return precio + precio * impuesto;
+}
+let resultado = precioCompleto(19.90, 0.15);
+console.log(resultado);
