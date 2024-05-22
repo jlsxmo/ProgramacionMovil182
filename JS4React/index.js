4d6f59e8e3a9232629915c7b8cae7531dcdc62ad
Juan Luis Mosqueda Orta <122045310@upq.edu.mx>
2024-05-22 14:56:17 -0600

    Fin práctica 2

--- JS4React/index.js
+++ JS4React/index.js
@@ -7,16 +7,17 @@ function suma(a, b) {
 console.log(suma(2, 3)); // 5*/
 
 //FatArrow function versión simplificada
-const suma = (a, b) => a + b;
+//const suma = (a, b) => a + b;
 
 /* También se puede escribir así:
 const suma = (a, b) => {
     return a + b;   
 }
 */
+/*
 console.log(suma(2, 3)); // 5
 
-const usuarios = {
+const usuario = {
     nombre: 'Ivan',
     apellido: 'Guerra',
     edad: 36,
@@ -27,4 +28,31 @@ const usuarios = {
     },
     amigos: ['Alex', 'Carlos'],
     estatus: true,
+    enviaCorreo: enviarCorr=()=> 'enviando...'
 };
+
+
+// Propiedades de un objeto
+//console.log(usuario);
+//console.log(usuario.nombre);
+//console.log(usuario.direccion.calle);
+//console.log(usuario.amigos);
+
+// Métodos de un objeto
+console.log(usuario.enviaCorreo());
+*/
+
+// Trabajo con el DOM
+const titulo = document.createElement('h1');
+titulo.innerText = 'Soy un título creado con JS';
+document.body.append(titulo);
+
+const boton = document.createElement('button');
+boton.innerText = 'Presioname';
+document.body.append(boton);
+
+boton.addEventListener('click', function() {
+    //console.log('Evento click ejecutado');
+    titulo.innerText = 'Evento click ejecutado';
+    alert('Exito');
+});
