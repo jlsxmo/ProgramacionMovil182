21668af5664804aa041f2a1b576f11f452a3c6f0
Juan Luis Mosqueda Orta <122045310@upq.edu.mx>
2024-05-24 15:32:57 -0600

    Práctica 3
    - Arreglos
    - Modulos: importaciones y exportaciones

--- JS4React/index.js
+++ JS4React/index.js
@@ -41,7 +41,7 @@ const usuario = {
 // Métodos de un objeto
 console.log(usuario.enviaCorreo());
 */
-
+/*
 // Trabajo con el DOM
 const titulo = document.createElement('h1');
 titulo.innerText = 'Soy un título creado con JS';
@@ -56,3 +56,63 @@ boton.addEventListener('click', function() {
     titulo.innerText = 'Evento click ejecutado';
     alert('Exito');
 });
+*/
+// Trabajo con Arreglos
+const nombres = ['alan', 'Isa', 'Pablo', 'Juan Luis'];
+/*
+// for clásico
+for (let i = 0; i < nombres.length; i++) {
+    console.log(nombres[i]);
+}
+
+// forEach
+nombres.forEach(function(nombre) {
+    console.log(nombre);
+});*/
+/*
+// Map sirve para recorrer un arreglo y devolver un nuevo arreglo
+const array2 = nombres.map(function(nombre) {
+    console.log(nombre);
+    return nombre;
+});
+console.log(nombres);
+console.log(array2);
+*/
+/*
+// Función find: Ubicar elementos en un arreglo
+const resultado = nombres.find(function(nombre) {
+    if (nombre === 'Pablo') {
+        return nombre;
+    }
+});
+console.log(resultado);
+*/
+
+/*
+// Función filter: Recorre el arreglo filtrando resultado en otro
+const resultado = nombres.filter(function(nombre) {
+   if (nombre != 'Pablo') {
+       return nombre;
+   }
+});
+console.log(resultado);
+console.log(nombres);
+*/
+
+/*
+// Función Concat: Concatena arreglos sin importar el tipo de dato
+const edades = [78, 89, 12, 8, 10, 15];
+console.log(nombres.concat(edades));
+
+// Spread Operator: Permite concatenar arreglos de una forma más sencilla
+console.log(...nombres, ...edades);
+*/
+
+
+// Módulos: Import & Export
+// Importar
+//import { suma, resta } from "./calculadora.js";
+import * as calc from "./calculadora.js";
+
+console.log(calc.suma(45, 54));
+console.log(calc.resta(45, 5));
