98177fb227d7424e823aa355ca50b01ce259b50d
Juan Luis Mosqueda Orta <122045310@upq.edu.mx>
2024-06-12 15:35:50 -0600

    Práctica 6

--- holaMundo/App.js
+++ holaMundo/App.js
@@ -1,15 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+
+const Texto = ({ contenido, actualizarContenido }) => {
+  return (<Text>{contenido}</Text>);
+};
 
 export default function App() {
+  const [contenido, setContenido] = useState('Hola mundo');
+  const actualizarContenido = () => {
+    setContenido('State actualizo este texto');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>¡Hola mundo!</Text>
-      <Button title='Presioname'></Button>
+      <Texto contenido={contenido}/>
+      
+      <Button title='Presioname' color="#000000" onPress={actualizarContenido} />
+      
       <StatusBar style="auto" />
     </View>
   );
 }
+
+/*import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import React, {useState} from 'react';
+
+const Texto = () => {
+  const [contenido, setContenido] = useState('Hola mundo');
+  const actualizarContenido = () => {
+    setContenido('State actualizo este texto');
+  }
+  return (<Text onPress={actualizarContenido}>{contenido}</Text>)
+}
+
+export default function App() {
+  return (    
+    <View style={styles.container}>
+
+      <Texto></Texto>
+      <Texto/>
+      
+      <Button title='Presioname' color="#000000"></Button>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}*/
 
 const styles = StyleSheet.create({
   container: {
