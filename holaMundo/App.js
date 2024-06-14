333147477fba31ca9b3686093cd3d0eb44c14ba9
Juan Luis Mosqueda Orta <122045310@upq.edu.mx>
2024-06-14 15:30:31 -0600

    Práctica 7
    StyleSheets y Flex

--- holaMundo/App.js
+++ holaMundo/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import React, { useState } from 'react';
 
+/*
 const Texto = ({ contenido, actualizarContenido }) => {
   return (<Text>{contenido}</Text>);
 };
@@ -22,38 +23,53 @@ export default function App() {
     </View>
   );
 }
+*/
 
-/*import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button} from 'react-native';
-import React, {useState} from 'react';
-
-const Texto = () => {
+const Texto = ({estilo}) => {
   const [contenido, setContenido] = useState('Hola mundo');
   const actualizarContenido = () => {
     setContenido('State actualizo este texto');
   }
-  return (<Text onPress={actualizarContenido}>{contenido}</Text>)
+  return (<Text style={[styles.text,estilo]} onPress={actualizarContenido}>{contenido}</Text>)
 }
 
 export default function App() {
   return (    
     <View style={styles.container}>
 
-      <Texto></Texto>
-      <Texto/>
-      
-      <Button title='Presioname' color="#000000"></Button>
+      <Texto estilo={styles.red}/>
+      <Texto estilo={styles.blue}/>
+      <Texto estilo={styles.green}/>
 
       <StatusBar style="auto" />
     </View>
   );
-}*/
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
+  },
+  text:{
+    color : 'yellow',
+    fontSize : 25,
+    height: 150,
+    //width: 150,
+  },
+  red:{
+    backgroundColor: 'red',
+    //flex: 1,
+  },
+  blue:{
+    backgroundColor: 'blue',
+    //flex: 2,
+  },
+  green:{
+    backgroundColor: 'green',
+    //flex: 3,
   },
 });
